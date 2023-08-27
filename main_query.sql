f9285055dc1d9b261ee4db858a8abcfbdc568efc
proman2520 <andrew.prozorovsky@gmail.com>
2023-08-27 15:09:15 -0500

    post data engineering

--- main_query.sql
+++ main_query.sql
@@ -1,47 +1,60 @@
+--Create departments table
 DROP TABLE if exists departments;
 CREATE TABLE departments (
-	dept_no VARCHAR,
-	dept_name VARCHAR
+	dept_no VARCHAR(4) PRIMARY KEY,
+	dept_name VARCHAR(20) NOT NULL
 );
 
-DROP TABLE if exists dept_emp;
-CREATE TABLE dept_emp (
-	emp_no INT,
-	dept_no VARCHAR
-);
-
-DROP TABLE if exists dept_manager;
-CREATE TABLE dept_manager (
-	dept_no VARCHAR,
-	emp_no INT
-);
-
-DROP TABLE if exists employees;
-CREATE TABLE employees (
-	emp_no INT,
-	emp_title_id VARCHAR,
-	birth_date DATE,
-	first_name VARCHAR,
-	last_name VARCHAR,
-	sex VARCHAR,
-	hire_date DATE
-);
-
-DROP TABLE if exists salaries;
-CREATE TABLE salaries (
-	emp_no INT,
-	salary INT
-);
-
+--Create titles table
 DROP TABLE if exists titles;
 CREATE TABLE titles (
-	title_id VARCHAR,
-	title VARCHAR
+	title_id VARCHAR(5) PRIMARY KEY,
+	title VARCHAR(20) NOT NULL
 );
 
+--Create employees table
+DROP TABLE if exists employees;
+CREATE TABLE employees (
+	emp_no INT NOT NULL PRIMARY KEY,
+	emp_title_id VARCHAR(5),
+	birth_date DATE,
+	first_name VARCHAR(30),
+	last_name VARCHAR(30),
+	sex VARCHAR(1),
+	hire_date DATE,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+--Create dept_emp table
+DROP TABLE if exists dept_emp;
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+--Create dept_manager table
+DROP TABLE if exists dept_manager;
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+--Create salaries table
+DROP TABLE if exists salaries;
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+--Example of table correctly created
 SELECT * from departments
 
-
+--Remember to load the csv data as well.
 
 
 

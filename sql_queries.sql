9ed1e9dfe966c6182a21f43c5cd5d1d5407dbe50
proman2520 <andrew.prozorovsky@gmail.com>
2023-08-28 17:44:08 -0500

    final

--- sql_queries.sql
+++ sql_queries.sql
@@ -1,5 +1,6 @@
 --List the employee number, last name, first name, sex, and salary of each employee
-SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary FROM employees
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary 
+FROM employees
 JOIN salaries
 ON employees.emp_no = salaries.emp_no;
 
